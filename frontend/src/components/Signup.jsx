7be04ea0a5b92d2d28118ad71e51d084eff01089
Eshita Islam <islam.eshita@gmail.com>
2023-10-23 21:08:47 -0700

    Finished the sign up page on the frontend

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,40 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
 function SignupForm() {
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
-  
-	const handleSignup = () => {
-		console.log(username, password);
-	};
-  
-	return (
-	  <div>
-		<h2>Sign Up</h2>
-		<form>
-		  <div>
-			<label htmlFor="username">Username</label>
-			<input
-			  type="text"
-			  id="username"
-			  value={username}
-			  onChange={(e) => setUsername(e.target.value)}
-			/>
-		  </div>
-		  <div>
-			<label htmlFor="password">Password</label>
-			<input
-			  type="password"
-			  id="password"
-			  value={password}
-			  onChange={(e) => setPassword(e.target.value)}
-			/>
-		  </div>
-		  <button type="button" onClick={handleSignup}>
-			Sign Up
-		  </button>
-		</form>
-	  </div>
-	);
-  }
-  
-  export default SignupForm;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    console.log(username, password, email, displayName);
+
+    await fetch(`http://localhost:3001/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        email: email,
+        display_name: displayName,
+      }),
+    });
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="displayName">Display Name</label>
+          <input
+            type="text"
+            id="displayName"
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button type="button" onClick={handleSignup}>
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default SignupForm;
