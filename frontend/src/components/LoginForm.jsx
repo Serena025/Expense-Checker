68352b096d2095b829c804cd96d8821ee3485889
Eshita Islam <islam.eshita@gmail.com>
2023-10-23 17:42:41 -0700

    Made the app have a react router dom

---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,5 @@
+function LoginForm() {    
+    return <h1>Login Form Goes Here</h1>
+}
+
+export default LoginForm
---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,5 @@
+function SignUpForm() {
+	return <h1>Sign up form goes here</h1>
+}
+
+export default SignUpForm
