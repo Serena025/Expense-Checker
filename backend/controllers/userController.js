a5a5eddfb6adfce657efac2d31e19018c40c6985
Eshita Islam <islam.eshita@gmail.com>
2023-10-23 20:36:51 -0700

    Functioning backend for signup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,8 +5,10 @@ const bcrypt = require("bcrypt");
 const { User } = db;
 
 // Route for adding a new user to the database
-router.post("/", async (req, res) => {
+userRouter.post("/", async (req, res) => {
   let { password, ...rest } = req.body;
+  console.log(req.body);
+  console.log(password);
   const user = await User.create({
     ...rest,
     password: await bcrypt.hash(password, 10),
@@ -15,7 +17,7 @@ router.post("/", async (req, res) => {
 });
 
 // Route for getting all users from the database
-router.get("/", async (req, res) => {
+userRouter.get("/", async (req, res) => {
   const users = await User.findAll();
   res.json(users);
 });
