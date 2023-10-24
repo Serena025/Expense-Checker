3ca1cb95b51179f6b5d161f4f123ae35414313cb
Eshita Islam <islam.eshita@gmail.com>
2023-10-23 19:42:45 -0700

    Changed the capitalization

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import "../styles/Navigation.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navigation.css";
 
 function Navigation() {
   return (
     <nav>
       <Link to="/">Home</Link>
-       <Link to="/dashboard">Dashboard</Link>
-       <Link to="/expenses">expenses</Link>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/expenses">Expenses</Link>
       <Link to="/login">Login</Link>
       <Link to="/signup">Sign up</Link>
       <Link to="/about">About</Link>
-     
     </nav>
   );
 }
