53db213512284c7742922edd2922cf98b1d53ee5
Eshita Islam <islam.eshita@gmail.com>
2023-10-25 20:48:23 -0700

    Linked up categories with the database

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,35 +1,88 @@
 const categoryRouter = require("express").Router();
+const db = require("../models");
 
-// Gets the list of all category
+const { Category } = db;
+
+// Gets all the categories
 categoryRouter.get("/", async (req, res) => {
-  const categories = [
-    {
-      id: 1,
-      name: "Housing",
-    },
-    {
-      id: 2,
-      source: "Travel",
-    },
-    {
-      id: 3,
-      name: "Food",
-    },
-    {
-      id: 4,
-      source: "Personal Care",
-    },
-  ];
+  let categories = await Category.findAll();
 
-  res.json(categories);
+  if (!categories) {
+    res.status(404).json({
+      message: `Could not retrieve categories`,
+    });
+  }
+
+  res.status(200).json(categories);
+});
+
+// Gets an individual category by id
+categoryRouter.get("/:id", async (req, res) => {
+  let categoryID = Number(req.params.id);
+  if (isNaN(categoryID)) {
+    res.status(404).json({ message: `Invalid id "${categoryID}"` });
+  } else {
+    const category = await Category.findOne({
+      where: { id: categoryID },
+    });
+
+    if (!category) {
+      res
+        .status(404)
+        .json({ message: `Could not find category with id "${categoryID}"` });
+    } else {
+      res.json(category);
+    }
+  }
 });
 
 // Adds a new category
+categoryRouter.post("/", async (req, res) => {
+  let category = await Category.create(req.body);
+  res.status(200).json(category);
+});
 
-// Gets an individual category by id
+// Update an existing category
+categoryRouter.put("/:id", async (req, res) => {
+  let categoryID = Number(req.params.id);
+  if (isNaN(categoryID)) {
+    res.status(404).json({ message: `Invalid id "${categoryID}"` });
+  } else {
+    const category = await Category.findOne({
+      where: { id: categoryID },
+    });
 
-// Route for updating a category
+    if (!category) {
+      res
+        .status(404)
+        .json({ message: `Could not find category with id "${categoryID}"` });
+    } else {
+      Object.assign(category, req.body);
+      await category.save();
+      res.json(category);
+    }
+  }
+});
 
-// Route for deleting a category
+// Delete a category
+categoryRouter.delete("/:id", async (req, res) => {
+  let categoryID = Number(req.params.id);
+  if (isNaN(categoryID)) {
+    res.status(404).json({ message: `Invalid id "${categoryID}"` });
+  } else {
+    const category = await Category.findOne({
+      where: { id: categoryID },
+    });
+
+    if (!category) {
+      res
+        .status(404)
+        .json({ message: `Could not find category with id "${categoryID}"` });
+    } else {
+      await category.destroy();
+      res.json(category);
+    }
+  }
+});
 
 module.exports = categoryRouter;
