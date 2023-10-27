1803712da61f3d039c3e7a1fd0c1b4d2c24da6be
Eshita Islam <islam.eshita@gmail.com>
2023-10-27 08:39:21 -0700

    Added the association between category and subcategory

--- backend/models/category.js
+++ backend/models/category.js
@@ -7,8 +7,12 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ SubCategory }) {
       // define association here
+      Category.hasMany(SubCategory, {
+        as: "subcategories",
+        foreignKey: "category_id",
+      });
     }
   }
   Category.init(
