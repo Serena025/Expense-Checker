cd9d80a16b28316e26fcb17840ca338592303e0c
dagui <davaguir852@gmail.com>
2023-10-28 16:59:59 -0700

    Dashbaord working forsure

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,89 +1,71 @@
 import React, { useState, useEffect } from 'react';
-import { Pie } from 'react-chartjs-2';
+import { Chart } from 'react-google-charts';
 import { getBackendURL } from '../common_functions';
-import "../styles/Dashboard.css";
+import '../styles/Dashboard.css';
 
 function Dashboard() {
   const [expenses, setExpenses] = useState([]);
   const [error, setError] = useState(null);
   const [chartData, setChartData] = useState(null);
 
-  const url = `${getBackendURL()}/expenses`;
-  useEffect(() => {
-    const fetchExpenses = async () => {
-      try {
-        const response = await fetch(url, {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-          },
-        });
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`${getBackendURL()}/expenses`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
 
-        if (!response.ok) {
-          throw new Error('Error fetching expenses');
-        }
-
-        const data = await response.json();
-        setExpenses(data);
-      } catch (err) {
-        console.error('Error fetching expenses data:', err);
-        setError(err.message);
+      if (!response.ok) {
+        throw new Error('Error fetching expenses');
       }
-    };
-    fetchExpenses();
-  }, [url]);
+
+      const data = await response.json();
+      setExpenses(data);
+    } catch (err) {
+      console.error('Error fetching expenses data:', err);
+      setError(err.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   useEffect(() => {
     if (expenses.length > 0) {
-      const data = generateChartData();
+      const categoryCounts = {};
+      for (const expense of expenses) {
+        if (categoryCounts[expense.category]) {
+          categoryCounts[expense.category] += 1;
+        } else {
+          categoryCounts[expense.category] = 1;
+        }
+      }
+      const data = [['Category', 'Count']];
+      for (const category in categoryCounts) {
+        data.push([category, categoryCounts[category]]);
+      }
       setChartData(data);
     }
   }, [expenses]);
-
-  const generateChartData = () => {
-    const categoryCounts = {};
-    for (const expense of expenses) {
-      if (categoryCounts[expense.category]) {
-        categoryCounts[expense.category] += 1;
-      } else {
-        categoryCounts[expense.category] = 1;
-      }
-    }
-
-    return {
-      labels: Object.keys(categoryCounts),
-      datasets: [
-        {
-          data: Object.values(categoryCounts),
-          backgroundColor: [
-            '#FF6384',
-            '#36A2EB',
-            '#FFCE56',
-            '#8e5ea2',
-            '#3cba9f',
-            '#e8c3b9',
-            '#c45850',
-          ],
-          hoverBackgroundColor: [
-            '#FF6384',
-            '#36A2EB',
-            '#FFCE56',
-            '#8e5ea2',
-            '#3cba9f',
-            '#e8c3b9',
-            '#c45850',
-          ],
-        },
-      ],
-    };
-  };
 
   return (
     <div>
       {error && <p>Error loading expenses: {error}</p>}
       {chartData && (
-        <div style={{ width: '400px', height: '400px' }}>
+        <div style={{ width: '800px', height: '600px' }}>
           <h2>Expense Categories</h2>
-          <Pie data={chartData} />
+          <Chart
+            chartType="PieChart"
+            width="100%"
+            height="100%"
+            data={chartData}
+            options={{
+              title: 'Expense Categories',
+              is3D: true,
+            }}
+          />
         </div>
       )}
     </div>
---
+++ frontend/src/components/EditEspense.jsx
@@ -0,0 +1,144 @@
+import React, { useState, useEffect } from "react";
+import { getBackendURL } from "../common_functions";
+
+function Expenses() {
+  const [expenses, setExpenses] = useState([]);
+  const [error, setError] = useState(null);
+  const [formData, setFormData] = useState({
+    date: "",
+    amount: "",
+    recipient: "",
+    description: "",
+  });
+
+  const url = `${getBackendURL()}/expenses`;
+  useEffect(() => {
+    const fetchExpenses = async () => {
+      try {
+        const response = await fetch(url, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Error fetching expenses");
+        }
+
+        const data = await response.json();
+        setExpenses(data);
+      } catch (err) {
+        console.error("Error fetching expenses data:", err);
+        setError(err.message);
+      }
+    };
+    fetchExpenses();
+  }, [url]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleAddExpense = async () => {
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error adding expense");
+      }
+
+      const newExpense = await response.json();
+      setExpenses([...expenses, newExpense]);
+      setFormData({
+        date: "",
+        amount: "",
+        recipient: "",
+        description: "",
+      });
+    } catch (err) {
+      console.error("Error adding expense:", err);
+      setError(err.message);
+    }
+  };
+
+  return (
+    <div className="expenses-1">
+      {error && <p>Error loading expenses: {error}</p>}
+      <form onSubmit={handleAddExpense}>
+        <input
+          type="text"
+          name="date"
+          value={formData.date}
+          onChange={handleInputChange}
+          placeholder="Date"
+        />
+        <input
+          type="text"
+          name="amount"
+          value={formData.amount}
+          onChange={handleInputChange}
+          placeholder="Amount"
+        />
+        <input
+          type="text"
+          name="recipient"
+          value={formData.recipient}
+          onChange={handleInputChange}
+          placeholder="Recipient / Merchant"
+        />
+        <input
+          type="text"
+          name="description"
+          value={formData.description}
+          onChange={handleInputChange}
+          placeholder="Description"
+        />
+        <button type="submit">Add Expense</button>
+      </form>
+      <table style={{ border: "1px solid black" }}>
+        <thead>
+          <tr striped>
+            <th style={{ border: "1px solid black", padding: "5px" }}>Date</th>
+            <th style={{ border: "1px solid black", padding: "5px" }}>
+              Amount
+            </th>
+            <th style={{ border: "1px solid black", padding: "5px" }}>
+              Recipient / Merchant
+            </th>
+            <th style={{ border: "1px solid black", padding: "5px" }}>
+              Description
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr key={expense.id}>
+              <td style={{ border: "1px solid black", padding: "5px" }}>
+                {expense.date}
+              </td>
+              <td style={{ border: "1px solid black", padding: "5px" }}>
+                ${expense.amount.toFixed(2)}
+              </td>
+              <td style={{ border: "1px solid black", padding: "5px" }}>
+                {expense.recipient}
+              </td>
+              <td style={{ border: "1px solid black", padding: "5px" }}>
+                {expense.description}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Expenses;
