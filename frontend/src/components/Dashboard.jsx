5a736d61be5467ed9ab46657c1ef69caa46359ab
dagui <davaguir852@gmail.com>
2023-10-28 13:33:38 -0700

    Dashboard pie chart ready

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,43 +1,93 @@
-import React from 'react';
-//import Table from 'react-bootstrap/Table';
+import React, { useState, useEffect } from 'react';
+import { Pie } from 'react-chartjs-2';
+import { getBackendURL } from '../common_functions';
 import "../styles/Dashboard.css";
 
-
-
-const expenses = [
-  { id: 1, description: 'Rent', amount: 1000, date: '2023-09-01' },
-  { id: 2, description: 'Groceries', amount: 100, date: '2023-09-05' },
-  { id: 3, description: 'Internet Bill', amount: 50, date: '2023-09-10' },
-  { id: 4, description: 'Dinner', amount: 200, date: '2023-09-15' },
-]
-
-
 function Dashboard() {
-    return (
-      <div>        
-        <h2>Dashboard</h2>
-        <table style={{ border: '1px solid black' }}>
-        <thead>
-          <tr striped>
-            <th style={{ border: '1px solid black', padding: '5px' }}>ID</th>
-            <th style={{ border: '1px solid black', padding: '5px' }}>Description</th>
-            <th style={{ border: '1px solid black', padding: '5px' }}>Amount</th>
-            <th style={{ border: '1px solid black', padding: '5px' }}>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {expenses.map((expense) => (
-            <tr key={expense.id}>
-              <td style={{ border: '1px solid black', padding: '5px' }}>{expense.id}</td>
-              <td style={{ border: '1px solid black', padding: '5px' }}>{expense.description}</td>
-              <td style={{ border: '1px solid black', padding: '5px' }}>{expense.amount}</td>
-              <td style={{ border: '1px solid black', padding: '5px' }}>{expense.date}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      </div>
-    );
-  }
+  const [expenses, setExpenses] = useState([]);
+  const [error, setError] = useState(null);
+  const [chartData, setChartData] = useState(null);
 
-  export default Dashboard;
+  const url = `${getBackendURL()}/expenses`;
+  useEffect(() => {
+    const fetchExpenses = async () => {
+      try {
+        const response = await fetch(url, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Error fetching expenses');
+        }
+
+        const data = await response.json();
+        setExpenses(data);
+      } catch (err) {
+        console.error('Error fetching expenses data:', err);
+        setError(err.message);
+      }
+    };
+    fetchExpenses();
+  }, [url]);
+
+  useEffect(() => {
+    if (expenses.length > 0) {
+      const data = generateChartData();
+      setChartData(data);
+    }
+  }, [expenses]);
+
+  const generateChartData = () => {
+    const categoryCounts = {};
+    for (const expense of expenses) {
+      if (categoryCounts[expense.category]) {
+        categoryCounts[expense.category] += 1;
+      } else {
+        categoryCounts[expense.category] = 1;
+      }
+    }
+
+    return {
+      labels: Object.keys(categoryCounts),
+      datasets: [
+        {
+          data: Object.values(categoryCounts),
+          backgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56',
+            '#8e5ea2',
+            '#3cba9f',
+            '#e8c3b9',
+            '#c45850',
+          ],
+          hoverBackgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56',
+            '#8e5ea2',
+            '#3cba9f',
+            '#e8c3b9',
+            '#c45850',
+          ],
+        },
+      ],
+    };
+  };
+
+  return (
+    <div>
+      {error && <p>Error loading expenses: {error}</p>}
+      {chartData && (
+        <div style={{ width: '400px', height: '400px' }}>
+          <h2>Expense Categories</h2>
+          <Pie data={chartData} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Dashboard;
