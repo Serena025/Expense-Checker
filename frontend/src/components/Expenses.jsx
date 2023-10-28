8525d19d30d01adfe9e4bdfd6fee253ed35d1bbf
Eshita Islam <islam.eshita@gmail.com>
2023-10-28 07:46:20 -0700

    A basic expense page

--- frontend/src/components/Expenses.jsx
+++ frontend/src/components/Expenses.jsx
@@ -1,18 +1,74 @@
-import "../styles/Expenses.css"
-import Button from 'react-bootstrap/Button'
-import Badge from 'react-bootstrap/Badge';
-import Nav from 'react-bootstrap/Nav'
-import Container from 'react-bootstrap/Container'
-
-
+import "../styles/Expenses.css";
+import { useState, useEffect } from "react";
+import { getBackendURL } from "../common_functions";
 
 function Expenses() {
-    return (
-      <div className="expenses-1">        
-        <h2>Expenses</h2> 
-    </div>
-     
-    );
-  }
+  const [expenses, setExpenses] = useState([]);
+  const [error, setError] = useState(null);
 
-  export default Expenses;
+  const url = `${getBackendURL()}/expenses`;
+  useEffect(() => {
+    const fetchExpenses = async () => {
+      try {
+        const response = await fetch(url, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Error fetching expenses");
+        }
+
+        const data = await response.json();
+        setExpenses(data);
+      } catch (err) {
+        console.error("Error fetching expenses data:", err);
+        setError(err.message);
+      }
+    };
+    fetchExpenses();
+  }, []);
+
+  return (
+    <div className="expenses-1">
+      {error && <p>Error loading expenses: {error}</p>}
+      <table style={{ border: "1px solid black" }}>
+        <thead>
+          <tr striped>
+            <th style={{ border: "1px solid black", padding: "5px" }}>Date</th>
+            <th style={{ border: "1px solid black", padding: "5px" }}>
+              Amount
+            </th>
+            <th style={{ border: "1px solid black", padding: "5px" }}>
+              Recipient / Merchant
+            </th>
+            <th style={{ border: "1px solid black", padding: "5px" }}>
+              Description
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr key={expense.id}>
+              <td style={{ border: "1px solid black", padding: "5px" }}>
+                {expense.date}
+              </td>
+              <td style={{ border: "1px solid black", padding: "5px" }}>
+                ${expense.amount.toFixed(2)}
+              </td>
+              <td style={{ border: "1px solid black", padding: "5px" }}>
+                {expense.recipient}
+              </td>
+              <td style={{ border: "1px solid black", padding: "5px" }}>
+                {expense.description}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Expenses;
